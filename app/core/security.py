67cf85b528249e60329d41b2c1278960135b8f46
Bora_blad <96587810+borablad@users.noreply.github.com>
2025-01-10 11:44:01 +0500

    start using settings

--- app/core/security.py
+++ app/core/security.py
@@ -1,10 +1,11 @@
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
+from app.core.config import settings
 
-SECRET_KEY = "your_secret_key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
--- app/db/session.py
+++ app/db/session.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from fastapi import Depends
+from app.core.config import settings
+
 
 # Настройка подключения к базе данных
-DATABASE_URL = "sqlite:///./auth.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 # Зависимость для получения сессии базы данных
 def get_db():
